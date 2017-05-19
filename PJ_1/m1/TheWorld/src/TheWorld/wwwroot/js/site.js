e5d9c03b7915c306c8385b6e464cbd7fb93974d3
paulohss <paulohss@gmail.com>
2017-05-19 15:51:12 +1000

    vv

---
+++ PJ_1/m1/TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("PH X");
+    
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on("mouseleave", function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    alert("Helloo");
+    //});
+
+    var $sideAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sideAndWrapper.toggleClass("hide-sidebar");
+    });
+     
+
+})();
